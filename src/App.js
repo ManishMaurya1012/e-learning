5487d0df36592f2cf3aa196d7b273b11d774aba1
ManishMaurya1012 <manishmauryaagr7071@gmail.com>
2025-01-13 22:13:55 +0530

    Upload

---
+++ src/App.js
@@ -0,0 +1,66 @@
+
+import './App.css';
+import React from 'react';
+import { apiUrl,filterData } from './data';
+import Navbar from './components/Navbar';
+import Filter from './components/Filter';
+import Cards from './components/Cards';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import Spinner from './components/Spinner';
+
+
+const App = () => {
+  const [courses,setCourses]=useState(null);
+  const [loading,setLoading]=useState(true);
+  const [category,setCategory]=useState(filterData[0].title);
+
+
+  async function fetchData(){
+    setLoading(true);
+      try{
+        const res=await fetch(apiUrl);
+        const output= await res.json();
+        //save data in to a variable
+        setCourses(output.data);
+      }
+      catch(error){
+        toast.error("Something Went Wrong");
+      }
+      setLoading(false);
+    }
+  
+  useEffect(()=>{
+    fetchData();
+  }, [])
+
+
+  return ( 
+    <div className='min-h-screen flex flex-col bg-[#1e1954]'>
+      <div>
+        <Navbar/>
+      </div>
+
+      <div className='bg-[#1e1954]'>
+          <div>
+            <Filter 
+            filterData={filterData} 
+              category={category}
+              setCategory={setCategory}
+            />
+          </div>
+
+          <div className='w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center min-h-[50vh]'>
+            {
+              loading ? (<Spinner/>) : (<Cards courses={courses} category={category}/>)
+            }
+          </div>
+      </div>
+
+     
+    </div>
+  );
+};
+
+export default App;
